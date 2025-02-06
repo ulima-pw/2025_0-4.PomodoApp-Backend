514c4bae718ae945bd75d45b888cdabdcba96737
Hernan Quintana <hernan@jsatch.com>
2025-02-06 12:46:13 -0500

    Solucionando lugar incorrecto de codigo

---
+++ src/Controllers/ProyectoController.ts
@@ -0,0 +1,49 @@
+import express, {Request, Response} from "express"
+
+const ProyectoController = () => {
+    const path = "/proyectos"
+
+    const router = express.Router()
+
+    // Operacion para listar proyectos
+    router.get("/", (req : Request, resp : Response ) => {
+        resp.json({
+            msg : "",
+            proyectos : [
+                {
+                    id : 1,
+                    nombre : "Proyecto 1",
+                    nro_pom : 5,
+                    categoria : 1,
+                    status : 1
+                },
+                {
+                    id : 2,
+                    nombre : "Proyecto 2",
+                    nro_pom : 2,
+                    categoria : 2,
+                    status : 1
+                }
+            ]
+        })
+    })
+
+    // Operacion para obtener un proyecto segun id
+    router.get("/:id", (req : Request, resp : Response ) => {
+        const id = req.params.id
+        resp.json({
+            msg : "",
+            proyecto : {
+                id : id,
+                nombre : "Proyecto 1",
+                nro_pom : 5,
+                categoria : 1,
+                status : 1
+            }
+        })
+    })
+
+    return { router, path }
+}
+
+export default ProyectoController;
